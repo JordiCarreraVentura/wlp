WordNet normalization and lemmatization; bug fixes

lib/FeatureExtractor.py
@@ -12,6 +12,17 @@ from nltk.corpus import stopwords
 
 from TermIndex import TermIndex
 
+#    Advanced features
+from Lemmatizer import PatternLemmatizer
+
+from WordNet import WordNet
+
+#from Collocations import Collocations
+
+#from WordEmbeddings import WordEmbeddings
+
+
+
 
 STOPWORDS = deft(bool)
 for w in stopwords.words('english'):
@@ -22,6 +33,7 @@ class FeatureExtractor:
     
     def __init__(
         self,
+        name,
         lowercase=True,
         lemmatize=False,
         rm_stopwords=False,
@@ -33,10 +45,13 @@ class FeatureExtractor:
         pos=False,
         sentiment=None,
         synsets=None,
-        embeddings=None
+        embeddings='',
+        min_df=3,
+        max_df=None
     ):
+        self.name = name
         self.lowercase = lowercase
-        self.lemmatize = lemmatize
+        self.lemmatizer = lemmatize
         self.rm_stopwords = rm_stopwords
         self.rm_numbers = rm_numbers
         self.rm_punct = rm_punct
@@ -46,8 +61,29 @@ class FeatureExtractor:
         self.pos = pos
         self.sentiment = sentiment
         self.synsets = synsets
+        self.generalized = deft(str)
         self.embeddings = embeddings
+        self.min_df = min_df
+        self.max_df = max_df
         #
+        self.index = TermIndex(u'')
+        self.tf = deft(Counter)
+        self.df = Counter()
+        self.mass = 0.0
+        self.n = 0
+        self.documents = []
+        self.__configure()
+    
+    def __str__(self):
+        return self.name
+    
+    def __configure(self):
+        if self.lemmatizer:
+            self.lemmatizer = PatternLemmatizer()
+        if self.synsets:
+            self.wordnet = WordNet()
+    
+    def clear(self):
         self.index = TermIndex(u'')
         self.tf = deft(Counter)
         self.df = Counter()
@@ -57,6 +93,7 @@ class FeatureExtractor:
     
     def update(self, tokens):
         preproced = []
+        tokens = self.lemmatize(tokens)
         for token in tokens:
             token = self.__preproc(token)
             if not token:
@@ -78,10 +115,27 @@ class FeatureExtractor:
             return None
         if self.lowercase:
             token = token.lower()
+        token = self.generalize(token)
         return token
     
     def compute(self):
-        return True
+        self.__fit_generalize()
+    
+    def __fit_generalize(self):
+        if not self.synsets:
+            return
+        for w in self.tf.keys():
+            generalized = self.wordnet.generalize(self.index[w])
+            i = self.index(generalized)
+            self.generalized[w] = i
+    
+    def generalize(self, token):
+        if not self.synsets:
+            return token
+        generalized = self.generalized[token]
+        if not generalized:
+            return token
+        return generalized
     
     def weight(self, i, w):
         if self._weight == 'tfidf':
@@ -103,6 +157,22 @@ class FeatureExtractor:
         doc = self.documents[i]
         vector = np.zeros(n_dim, dtype=float)
         for w in doc:
+            if self.__out_of_bounds(w):
+                continue
             weight = self.weight(i, w)
             vector[w] = weight
         return vector
+    
+    def __out_of_bounds(self, w):
+        if self.min_df and self.df[w] < self.min_df:
+            return True
+        elif self.max_df and self.df[w] > self.max_df:
+            return True
+        else:
+            return False
+    
+    def lemmatize(self, tokens):
+        if not self.lemmatizer:
+            return tokens
+        lemmatized = self.lemmatizer(' '.join(tokens))
+        return lemmatized